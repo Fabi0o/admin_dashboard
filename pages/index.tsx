1916658802f48283e7df010aa9926c1255ac3945
Fabi0o <marfab32@gmail.com>
2023-03-13 18:45:19 +0100

    add sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/Home.module.scss";
+import { useState } from "react";
 
-export default function Home() {
-  return <></>;
+export default function Home({ data }: any) {
+  const [allCarts, setAllCarts] = useState(data.carts);
+  return <div className={styles.container}></div>;
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://dummyjson.com/carts");
+  const data = await res.json();
+
+  return { props: { data } };
 }
