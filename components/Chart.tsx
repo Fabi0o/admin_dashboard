5254925a4b86a108ffb6718fe5041172f25d72d5
Fabi0o <marfab32@gmail.com>
2023-03-15 18:33:59 +0100

    change chart labels

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -10,10 +10,12 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
+
 const Chart: React.FC<CurrentCartProps> = ({ currentCart }) => {
+  console.log(currentCart);
   const data = currentCart.products.map((data) => {
     let newdata = new ChartData(
-      `${data.id}`,
+      `Product ID ${data.id}`,
       data.price,
       Math.floor((data.discountedPrice / data.quantity) * 100) / 100
     );
@@ -26,10 +28,16 @@ const Chart: React.FC<CurrentCartProps> = ({ currentCart }) => {
       <LineChart width={600} height={300} data={data}>
         <Line type="monotone" dataKey="RegularPrice" stroke="#8884d8" />
         <Line type="monotone" dataKey="DiscountedPrice" stroke="#37ff2c" />
+
         <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="name" padding={{ left: 30, right: 30 }} />
+        <XAxis
+          dataKey="name"
+          padding={{ left: 30, right: 30 }}
+          fontSize={"12px"}
+        />
         <YAxis />
         <Tooltip />
+
         <Legend />
       </LineChart>
     </div>
