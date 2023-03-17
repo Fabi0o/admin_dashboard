f3cca78be181478872a7e82ebb110cee73b79b25
Fabi0o <marfab32@gmail.com>
2023-03-17 18:24:35 +0100

    form functionality

--- components/NewCartForm.tsx
+++ components/NewCartForm.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/NewCartForm.module.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   setDisplayForm: React.Dispatch<React.SetStateAction<boolean>>;
@@ -9,6 +9,10 @@ const NewCartForm: React.FC<Props> = ({ setDisplayForm }) => {
   const chart = document.querySelector(".recharts-wrapper");
   let container: HTMLElement;
 
+  const [userID, setUserID] = useState<number>();
+  const [productID, setProductID] = useState<number>();
+  const [products, setProducts] = useState<number[]>([]);
+
   useEffect(() => {
     container = document.querySelector(`.${styles.container}`)!;
     chart?.classList.add(`${styles.isHidden}`);
@@ -17,6 +21,31 @@ const NewCartForm: React.FC<Props> = ({ setDisplayForm }) => {
   const closeForm = () => {
     chart?.classList.remove(`${styles.isHidden}`);
     setDisplayForm(false);
+  };
+
+  const addProducts = () => {
+    const error = document.querySelector("#errorMess")!;
+    const input = document.querySelector("#productID") as HTMLInputElement;
+    if (!productID) {
+      error.textContent = "Fill product ID field!";
+      return;
+    }
+    if (products.includes(productID)) {
+      error.textContent = "You can't add the same product twice!";
+      return;
+    }
+    if (products.length >= 5) {
+      error.textContent = "You can't add more than 5 products to 1 cart!";
+      input.value = "";
+      input.disabled = true;
+      return;
+    }
+
+    if (error.textContent) error.textContent = "";
+    setProducts(products ? [...products!, productID] : [productID]);
+    setProductID(undefined);
+
+    input.value = "";
   };
 
   return (
@@ -33,15 +62,42 @@ const NewCartForm: React.FC<Props> = ({ setDisplayForm }) => {
 
         <div className={styles.inputContainer}>
           <label htmlFor="userID">User ID</label>
-          <input type="number" id="userID" />
+          <input
+            type="number"
+            id="userID"
+            onChange={(e) => setUserID(Number(e.target.value))}
+          />
         </div>
 
         <div className={styles.inputContainer}>
           <label htmlFor="productID">Product ID</label>
-          <input type="number" />
+          <input
+            type="number"
+            id="productID"
+            onChange={(e) => {
+              if (Number(e.target.value) < 0) e.target.value = "0";
+              if (Number(e.target.value) > 100) e.target.value = "100";
+              setProductID(Number(e.target.value));
+            }}
+          />
+          <div id="errorMess" className={styles.error}></div>
         </div>
 
-        <button type="button" className={styles.actionButton}>
+        {products && (
+          <div>
+            {products.map((product) => (
+              <div key={products.indexOf(product)}>
+                Product {products.indexOf(product) + 1}: ID {product}
+              </div>
+            ))}
+          </div>
+        )}
+
+        <button
+          type="button"
+          className={styles.actionButton}
+          onClick={addProducts}
+        >
           Add Product
         </button>
 
