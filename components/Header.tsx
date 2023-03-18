4512190d9f077a3b8871b2bde060d85b01b6c7f0
Fabi0o <marfab32@gmail.com>
2023-03-18 15:26:29 +0100

    header fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,11 @@ import styles from "@/styles/Header.module.scss";
 const Header: React.FC<CartProps> = ({ allCarts }) => {
   return (
     <header className={styles.header}>
-      <h1>Hello Admin! There are {allCarts.length} carts</h1>
+      {allCarts.length > 1 && (
+        <h1>Hello Admin! There are {allCarts.length} carts</h1>
+      )}
+      {allCarts.length == 1 && <h1>Hello Admin! There is 1 cart</h1>}
+      {!allCarts[0] && <h1>Hello Admin! There are 0 carts</h1>}
     </header>
   );
 };
