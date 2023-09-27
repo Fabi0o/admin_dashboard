97b892ae79b5448f9f9a413d332564f4721d02f9
Fabi0o <marfab32@gmail.com>
2023-09-27 12:49:16 +0200

    fix header bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,10 @@ const Header: React.FC<CartProps> = ({ allCarts }) => {
   return (
     <header className={styles.header}>
       {allCarts.length > 1 && (
-        <h1>Hello Admin! There are {allCarts.length} carts</h1>
+        <h1>
+          Hello Admin! There are{" "}
+          {allCarts.filter((cart) => !cart.isDeleted).length} carts
+        </h1>
       )}
       {allCarts.length == 1 && <h1>Hello Admin! There is 1 cart</h1>}
       {!allCarts[0] && <h1>Hello Admin! There are 0 carts</h1>}
